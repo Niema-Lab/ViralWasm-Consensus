692016784ff5652a88daa3ff2d24f2c74398cf87
Daniel Ji <jidaniel1234@gmail.com>
2023-09-10 18:11:55 -0700

    attempt to fix workflow

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  }
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		base: env.VITE_OFFLINE_VERSION === "true" ? '/' : '/ViralWasm-Consensus/',
+		plugins: [react()],
+		build: {
+			sourcemap: true,
+		}
+	}
 })
