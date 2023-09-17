5ef4be35640ad52d45136c95f777f8df5d807a59
Daniel Ji <jidaniel1234@gmail.com>
2023-09-17 02:24:36 -0700

    fix test

--- e2e/tests/pageload.spec.ts
+++ e2e/tests/pageload.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const url = 'http://localhost:8000';
+const url = 'http://localhost:5173';
 
 test('page load', async ({ page }) => {
 	const errors: string[] = [];
