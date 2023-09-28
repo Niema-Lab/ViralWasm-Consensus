507090cb599a7c77d07d42ec3668cb78aa748d2f
Daniel Ji <jidaniel1234@gmail.com>
2023-09-28 14:29:20 -0700

    test benchmarks

--- e2e/tests/run-baseline-benchmarks.sh
+++ e2e/tests/run-baseline-benchmarks.sh
@@ -4,12 +4,12 @@ cd ../data
 {
   time {
     minimap2 -t 1 -a -o reads.sam NC_045512.2.fas reads.fastq.gz && \
-    viral_consensus -i reads.sam -r NC_045512.2.fas -o consensus.fa -q 20 -d 10 -f 0.5 -a N
+    viral_consensus -i reads.sam -r NC_045512.2.fas -o ../../benchmarks/consensus.fa -q 20 -d 10 -f 0.5 -a N
   }
 } 2> time_output.log
 
 # Extract real time from the time output and append/write to full-run-time.log
-grep "real" time_output.log | awk -F'[ms]' '{print $2 + $3}' > full-run-time.log
+grep "real" time_output.log | awk -F'[ms]' '{print $2 + $3}' > ../../benchmarks/full-run-time.log
 
 rm time_output.log
 rm reads.sam
